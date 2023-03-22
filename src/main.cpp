23ebfd0f1cec26f74f4d414b65fdbfe3be31f7c6
llida01 <126851615+llida01@users.noreply.github.com>
2023-03-22 15:47:37 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include "Automata.h"
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string menu[] = { "Espresso", "Capuccino", "Latte", "Americano", "Raf", "Hot chocolate", "Cocoa"};
+  int prices[] = {70, 140, 140, 130, 190, 160, 140};
+  Automata automat = Automata(menu, prices);
+  automat.on();
+  automat.coin(140);
+  automat.choice(2);
+  automat.cancel();
+  automat.coin(160);
+  automat.choice(5);
+  automat.check();
+  automat.cook();
+  automat.finish();
+  automat.off();
+}
